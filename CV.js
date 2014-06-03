4e0d5be13725ad0404a838dae75f91bc1e3050b3
Jagoba <avalonfreeman@hotmail.com>
2014-06-03 21:11:23 +0200

    1st commit

---
+++ CV.js
@@ -0,0 +1,28 @@
+/*navigation header*/
+$(document).on('scroll',function(){
+    		    		
+    		// menu show and hidde
+    		
+		    if(($(document).scrollTop()+1)>($("#Idcontent1").height())){
+		        $("header").removeClass("hhidden").addClass("hshowed");
+		    } else{
+		        $("header").removeClass("hshowed").addClass("hhidden");
+		    }
+	});
+
+/*smooth scrolling */
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
