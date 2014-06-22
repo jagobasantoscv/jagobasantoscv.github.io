fe134d3b8db5ee6ddc5d18ed959f5ec7571c35b5
Jagoba <avalonfreeman@hotmail.com>
2014-06-22 22:34:17 +0200

    map

--- CV.js
+++ CV.js
@@ -4,18 +4,16 @@ var scroll_timer;
     var $window = $(window);
     var top = $(document.body).children(0).position().top;
 /*navigation header*/
-$(document).on('scroll',function(){
-                
-        // menu show and hidde       
-        if(($(document).scrollTop()+1)>($("#Idcontent1").height())){
-            $("header").removeClass("hhidden").addClass("hshowed");
-        } else{
-            $("header").removeClass("hshowed").addClass("hhidden");
-        }
+$(document).on('scroll',function(){             
+  // menu show and hidde       
+  if(($(document).scrollTop()+1)>($("#Idcontent1").height())){
+    $("header").removeClass("hhidden").addClass("hshowed");
+  } else{
+    $("header").removeClass("hshowed").addClass("hhidden");
+  }
   });
 
 /*smooth scrolling */
-
 $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -39,6 +37,7 @@ function parallax(){
   var scrolled = $(window).scrollTop();
   $('.bg').css('top',-(scrolled*0.2)+'px');
 }
+
 /*Loading*/
 $(window).load(function() {
   $(".loader").fadeOut("slow");
@@ -46,14 +45,12 @@ $(window).load(function() {
 
 /*maps*/
 function initialize() {
-        var map_canvas = document.getElementById('map');
-        var map_options = {
-          center: new google.maps.LatLng(47.412400, 9.743790),
-          zoom: 12,
-          mapTypeId: google.maps.MapTypeId.ROADMAP,
-        }
-        var map = new google.maps.Map(map_canvas, map_options)
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);
-
-
+  var map_canvas = document.getElementById('map');
+  var map_options = {
+    center: new google.maps.LatLng(47.412400, 9.743790),
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+  }
+  var map = new google.maps.Map(map_canvas, map_options)
+  }
+google.maps.event.addDomListener(window, 'load', initialize);
